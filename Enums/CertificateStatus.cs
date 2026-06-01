eb1c079a2e23a2ecbc073bb66ed318dafd9ff3b5
CoffeeFriendly <NikitinDanV@gmail.com>
2026-06-01 20:16:58 +0300

    Init

---
+++ Enums/CertificateStatus.cs
@@ -0,0 +1,11 @@
+namespace CertificatesApp.Enums
+{
+    public enum CertificateStatus
+    {
+        created = 1,
+        inProgress = 2,
+        ready = 3,
+        completed = 4,
+        cancelled = 5
+    }
+}
---
+++ Enums/CertificateTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace CertificatesApp.Enums
+{
+    public enum CertificateTypes
+    {
+        NDFL = 1,
+        workRecord = 2,
+        averageSalary = 3,
+        custom = 4
+    }
+}
