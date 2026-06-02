825af0111e186cc21c1cc575568abe77f60c73b2
CoffeeFriendly <NikitinDanV@gmail.com>
2026-06-03 01:53:11 +0300

    add Readme.md

--- obj/Debug/net10.0/CertificatesApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/CertificatesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertificatesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81eb93563fbc8c1cddd34106f883a9e5638f9479")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5210d5b5506e7087ca8724f214c2ae0dee2105")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertificatesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertificatesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
