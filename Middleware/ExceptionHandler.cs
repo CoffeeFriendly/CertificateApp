84123690187135ed42d942fd926738da93ca726b
CoffeeFriendly <NikitinDanV@gmail.com>
2026-06-02 16:12:17 +0300

    Add middleware

---
+++ Middleware/ExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CertificatesApp.Middleware
+{
+    public class ExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<ExceptionHandler> _logger;
+        
+        public ExceptionHandler(ILogger<ExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, 
+            Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "Произошла ошибка: {message}", exception.Message);
+
+            // ЗАМЕНИТЬ после создания кастомных исключений!!!
+            var (statusCode, title) = exception switch
+            {
+                Exception => (StatusCodes.Status403Forbidden, "Error placeholder")
+            };
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = title,
+                Detail = exception.Message,
+                Instance = context.Request.Path
+            };
+
+            context.Response.StatusCode = statusCode;
+            await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+            return true;
+        }
+    }
+}
--- obj/Debug/net10.0/CertificatesApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/CertificatesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertificatesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35ae220c308824a99123669def10a4a5e12a75c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1519d50a52c9c8502190d733895a2a5ec9928904")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertificatesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertificatesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
